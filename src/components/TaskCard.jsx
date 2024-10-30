c5c2b5e498fe5b803309ba5caf33b6c85f39bc7d
SohamMore100 <soham.more16451@sakec.ac.in>
2024-10-30 12:26:39 +0530

    "Formatted code in TaskCard, TaskForm, and main files to improve readability"

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,9 +1,9 @@
-import React,{useState} from 'react';
+import React, { useState } from "react";
 import { FaRegStar } from "react-icons/fa";
 import { HiTrash } from "react-icons/hi2";
 import { HiDotsVertical } from "react-icons/hi";
 import { IoCalendarOutline } from "react-icons/io5";
-import ConfirmationDialog from './ConfirmationDialog';
+import ConfirmationDialog from "./ConfirmationDialog";
 import DatePicker from "react-datepicker";
 
 const TaskCard = ({ task, onUpdateStatus, onDeleteTask, onUpdateTask }) => {
@@ -16,7 +16,12 @@ const TaskCard = ({ task, onUpdateStatus, onDeleteTask, onUpdateTask }) => {
   const [editCompleted, setEditCompleted] = useState(task.completed);
 
   const handleSave = () => {
-    onUpdateTask(task.id, { title: editTitle, description: editDescription, dueDate: editDueDate, completed: editCompleted });
+    onUpdateTask(task.id, {
+      title: editTitle,
+      description: editDescription,
+      dueDate: editDueDate,
+      completed: editCompleted,
+    });
     setIsEditing(false);
   };
 
@@ -29,8 +34,12 @@ const TaskCard = ({ task, onUpdateStatus, onDeleteTask, onUpdateTask }) => {
   };
 
   return (
-<>
-      <div className={`p-4 rounded-lg shadow ${task.completed ? 'bg-red-100' : 'bg-white'} h-[102%] w-[95%]`}>
+    <>
+      <div
+        className={`p-4 rounded-lg shadow ${
+          task.completed ? "bg-red-100" : "bg-white"
+        } h-[102%] w-[95%]`}
+      >
         {isEditing ? (
           <>
             <input
@@ -46,7 +55,6 @@ const TaskCard = ({ task, onUpdateStatus, onDeleteTask, onUpdateTask }) => {
             />
             <input
               type="date"
-              
               value={editDueDate}
               onChange={(e) => setEditDueDate(e.target.value)}
               className="w-full mb-2 p-2 border rounded"
@@ -61,44 +69,97 @@ const TaskCard = ({ task, onUpdateStatus, onDeleteTask, onUpdateTask }) => {
               />
             </label>
             <div className="flex justify-between mt-2">
-              <button onClick={handleSave} className="bg-blue-500 font-medium text-white px-3 py-2 rounded-md">
+              <button
+                onClick={handleSave}
+                className="bg-blue-500 font-medium text-white px-3 py-2 rounded-md"
+              >
                 Save
               </button>
-              <button onClick={() => setIsEditing(false)} className="bg-gray-500 font-medium text-white px-3 py-2 rounded-md">
+              <button
+                onClick={() => setIsEditing(false)}
+                className="bg-gray-500 font-medium text-white px-3 py-2 rounded-md"
+              >
                 Cancel
               </button>
             </div>
           </>
         ) : (
           <>
-            <h3 className={`font-bold text-lg ${task.completed ? 'text-red-500' : 'text-gray-600 dark:text-black'} pb-4`}>
+            <h3
+              className={`font-bold text-lg ${
+                task.completed
+                  ? "text-red-500"
+                  : "text-gray-600 dark:text-black"
+              } pb-4`}
+            >
               {task.title}
             </h3>
-            <p className={`font-medium text-base ${task.completed ? 'text-red-500' : 'text-gray-500 dark:text-gray-700'} pb-20`}>
+            <p
+              className={`font-medium text-base ${
+                task.completed
+                  ? "text-red-500"
+                  : "text-gray-500 dark:text-gray-700"
+              } pb-20`}
+            >
               {task.description}
             </p>
-            <div className='flex gap-3'>
-              <IoCalendarOutline className={`text-2xl ${task.completed ? 'text-red-500' : 'text-gray-500 dark:text-gray-900'}`} />
-              <p className={`text-lg font-semibold ${task.completed ? 'text-red-500' : 'text-gray-500 dark:text-gray-950'}`}>
+            <div className="flex gap-3">
+              <IoCalendarOutline
+                className={`text-2xl ${
+                  task.completed
+                    ? "text-red-500"
+                    : "text-gray-500 dark:text-gray-900"
+                }`}
+              />
+              <p
+                className={`text-lg font-semibold ${
+                  task.completed
+                    ? "text-red-500"
+                    : "text-gray-500 dark:text-gray-950"
+                }`}
+              >
                 {task.dueDate}
               </p>
             </div>
-            <hr className={`mt-4 mb-5 border-solid border rounded-lg w-full  ${task.completed ? 'border-red-500' : 'border-dashed border-gray-300 dark:border-gray-500'}`} />
+            <hr
+              className={`mt-4 mb-5 border-solid border rounded-lg w-full  ${
+                task.completed
+                  ? "border-red-500"
+                  : "border-dashed border-gray-300 dark:border-gray-500"
+              }`}
+            />
             <div className="flex justify-between mt-2">
-              <span className={` font-bold text-base ${task.completed ? 'text-red-500' : 'text-gray-500 dark:text-gray-900'}`}>
-                {task.completed ? 'Completed' : 'Incompleted'}
+              <span
+                className={` font-bold text-base ${
+                  task.completed
+                    ? "text-red-500"
+                    : "text-gray-500 dark:text-gray-900"
+                }`}
+              >
+                {task.completed ? "Completed" : "Incompleted"}
               </span>
-              <div className="flex space-x-4">   
+              <div className="flex space-x-4">
                 {!task.completed && (
-                  <FaRegStar onClick={onUpdateStatus} className={`text-2xl text-gray-500 dark:text-gray-900`}/>
+                  <FaRegStar
+                    onClick={onUpdateStatus}
+                    className={`text-2xl text-gray-500 dark:text-gray-900`}
+                  />
                 )}
                 <HiTrash
                   onClick={() => setIsDialogOpen(true)}
-                  className={`text-2xl ${task.completed ? 'text-red-500' : 'text-gray-500 dark:text-gray-900'}`}
+                  className={`text-2xl ${
+                    task.completed
+                      ? "text-red-500"
+                      : "text-gray-500 dark:text-gray-900"
+                  }`}
                 />
                 <HiDotsVertical
                   onClick={() => setIsEditing(!isEditing)}
-                  className={`text-2xl ${task.completed ? 'text-red-500' : 'text-gray-500 dark:text-gray-900'}`}
+                  className={`text-2xl ${
+                    task.completed
+                      ? "text-red-500"
+                      : "text-gray-500 dark:text-gray-900"
+                  }`}
                 />
               </div>
             </div>
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,66 +1,71 @@
-import React, { useState } from 'react';
-import moment from 'moment';
+import React, { useState } from "react";
+import moment from "moment";
 
 const TaskForm = ({ handleAddTask, setShowForm }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [dueDate, setDueDate] = useState('');
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
   const [completed, setCompleted] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formattedDate = moment(dueDate).format('DD/MM/YYYY');
+    const formattedDate = moment(dueDate).format("DD/MM/YYYY");
     // Save the task with the formatted date
     handleAddTask({ title, description, dueDate: formattedDate, completed });
     setShowForm(false); // Close the form after adding the task
   };
 
   return (
-    <form onSubmit={handleSubmit} className='p-3'>
-        <input
-          type="text"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-          className="w-full mb-2 p-1 border rounded-md dark:text-gray-900"
-          placeholder="Enter task title"
-          required
-        />
+    <form onSubmit={handleSubmit} className="p-3">
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        className="w-full mb-2 p-1 border rounded-md dark:text-gray-900"
+        placeholder="Enter task title"
+        required
+      />
 
-        <textarea value={description}
-          onChange={(e) => setDescription(e.target.value)}
-          className="w-full mb-2 p-1 border rounded-md dark:text-gray-900"
-          placeholder="Enter description"
-          required
-        />
+      <textarea
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        className="w-full mb-2 p-1 border rounded-md dark:text-gray-900"
+        placeholder="Enter description"
+        required
+      />
 
-        <input
-          type="date"
-          value={dueDate}
-          onChange={(e) => setDueDate(e.target.value)}
-          className="w-full mb-2 p-1 border rounded-md dark:text-gray-900"
-          required
-        />
+      <input
+        type="date"
+        value={dueDate}
+        onChange={(e) => setDueDate(e.target.value)}
+        className="w-full mb-2 p-1 border rounded-md dark:text-gray-900"
+        required
+      />
 
-        <label className="text-gray-700 font-semibold dark:text-gray-100">
-          Completed:
-        </label>
-        <input
-          type="checkbox"
-          checked={completed}
-          onChange={(e) => setCompleted(e.target.checked)}
-          className="ml-2 mr-2"
-        />
-        <span>{completed ? "Yes" : "No"}</span>
+      <label className="text-gray-700 font-semibold dark:text-gray-100">
+        Completed:
+      </label>
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={(e) => setCompleted(e.target.checked)}
+        className="ml-2 mr-2"
+      />
+      <span>{completed ? "Yes" : "No"}</span>
 
       <div className="flex justify-between mt-1">
         <button
           type="button"
           onClick={() => setShowForm(false)}
-          className="bg-blue-500 text-white font-medium px-4 py-1 rounded-md dark:bg-zinc-100 dark:text-blue-600 ">Cancel
+          className="bg-blue-500 text-white font-medium px-4 py-1 rounded-md dark:bg-zinc-100 dark:text-blue-600 "
+        >
+          Cancel
         </button>
         <button
           type="submit"
-          className="bg-green-500 text-white font-medium px-4 py-1 rounded-md dark:bg-zinc-100 dark:text-green-600 dark:font-semibold">Add Task
+          className="bg-green-500 text-white font-medium px-4 py-1 rounded-md dark:bg-zinc-100 dark:text-green-600 dark:font-semibold"
+        >
+          Add Task
         </button>
       </div>
     </form>
